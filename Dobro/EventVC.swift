be38ae08aebaae957b7451597f33d5ade3b3b777
Ingwar <Dev1@Dev1s-iMac.local>
2016-01-14 11:34:07 +0200

    last words

--- Dobro/EventVC.swift
+++ Dobro/EventVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 class EventVC: UIViewController {
 
@@ -15,8 +16,24 @@ class EventVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.parseEvent()
         
+    }
+    
+    func parseEvent() {
+        if postKey != nil {
+            let PostsQuery: PFQuery =  PFQuery(className:"Post")
+            PostsQuery.includeKey("category")
+            PostsQuery.getObjectInBackgroundWithId(postKey! as String, block: { (object, error) -> Void in
+                if error == nil {
+                    let key = object!.objectId as String!
+                    let date = object!.createdAt as NSDate!
+                    let post = Post(postKey: key, date: date, dictionary: object!)
+                    print(post)
+                }
+            })
+            
+        }
     }
 
 
